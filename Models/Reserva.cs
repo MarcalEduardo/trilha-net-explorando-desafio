dbea93017d5133a9301e088d46dcc77ef5295377
Eduardo Marcal <emclemente@outlook.com.br>
2024-05-06 19:15:43 -0300

    Concluido Desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+using System;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -16,15 +17,15 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+       
+            if ( DiasReservados >=hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException ("Suite não comporta a quantidade de hospedes informado");
             }
         }
 
@@ -36,22 +37,22 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            var retornaQuantidadeHospedes = Hospedes.Count;
+            return retornaQuantidadeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            var calculo= DiasReservados * Suite.ValorDiaria;
+            decimal valor = calculo;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+               var valorComDesconto = (double)DiasReservados * (double)Suite.ValorDiaria * 0.9;
+               valor = (decimal)valorComDesconto;
             }
 
             return valor;
